f017fb6c4d5a3abc60d5e249f63ec53f338ef09b
Cemil Aydıngöz <cemil.aydingoz@patikaglobal.com>
2023-10-20 21:20:47 +0300

    sidebar fold revert

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { CContainer, CHeader, CHeaderBrand, CHeaderNav, CNavLink, CNavItem } from '@coreui/react'
+import { useSelector, useDispatch } from 'react-redux'
+import { CContainer, CHeader, CHeaderBrand, CHeaderNav, CNavLink, CNavItem, CHeaderToggler } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilEnvelopeOpen } from '@coreui/icons'
+import { cilEnvelopeOpen, cilMenu } from '@coreui/icons'
 
 import { AppBreadcrumb, AppHeaderDropdown } from './index'
 import { logo } from 'src/assets/brand/logo'
 
 const AppHeader = () => {
+  const dispatch = useDispatch()
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+
   return (
     <CHeader position="sticky" className="mb-4">
       <CContainer fluid>
+        <CHeaderToggler className="ps-1" onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}>
+          <CIcon icon={cilMenu} size="lg" />
+        </CHeaderToggler>
         <CHeaderBrand className="mx-auto d-md-none" to="/">
           <CIcon icon={logo} height={48} alt="Logo" />
         </CHeaderBrand>
