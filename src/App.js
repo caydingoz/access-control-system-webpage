b3b7fed1dbb6162af5e4ea94bbbf36c1a10537fe
Cemil Aydıngöz <cemil.aydingoz@patikaglobal.com>
2023-10-24 13:09:53 +0300

    typo

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ const loading = (
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+const Anonymous = React.lazy(() => import('./helpers/Anonymous'))
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
-const Anonymous = React.lazy(() => import('./helpers/Anonymous'))
 
 //Buraya eklenen componentler full sayfa açılıyor.
 class App extends Component {
