1fb549d5bafa266961ac2f328cfa5c2ea1942c4d
caydingoz <caydingoz1@gmail.com>
2024-08-07 01:21:23 +0300

    Login page changed.

--- src/views/login-register/Login.js
+++ src/views/login-register/Login.js
@@ -1,111 +1,208 @@
-import { React, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { CButton, CCard, CCardBody, CCardGroup, CCol, CContainer, CForm, CFormInput, CInputGroup, CInputGroupText, CRow, CAlert } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
+import * as React from 'react'
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles'
+import GlobalStyles from '@mui/joy/GlobalStyles'
+import CssBaseline from '@mui/joy/CssBaseline'
+import Box from '@mui/joy/Box'
+import Button from '@mui/joy/Button'
+import Checkbox from '@mui/joy/Checkbox'
+import FormControl from '@mui/joy/FormControl'
+import FormLabel from '@mui/joy/FormLabel'
+import IconButton from '@mui/joy/IconButton'
+import Link from '@mui/joy/Link'
+import Input from '@mui/joy/Input'
+import Typography from '@mui/joy/Typography'
+import Stack from '@mui/joy/Stack'
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded'
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded'
+import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded'
+import { useNavigate } from 'react-router-dom'
+import { useColorModes } from '@coreui/react'
 
 import AuthService from '../../services/AuthService'
 
-const Login = () => {
-  const authService = AuthService()
-  const navigate = useNavigate()
-  const [errorInfo, setErrorInfo] = useState({
-    error: false,
-    errorMessage: '',
-  })
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-  })
-  function handleChange(evt) {
-    const value = evt.target.value
-    setForm({
-      ...form,
-      [evt.target.name]: value,
-    })
-  }
+function ColorSchemeToggle(props) {
+  const { onClick, ...other } = props
+  const { mode, setMode } = useColorScheme()
+  const { setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => setMounted(true), [])
+
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput type="text" placeholder="Email" autoComplete="email" name="email" value={form.email} onChange={handleChange} />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="password"
-                        name="password"
-                        value={form.password}
-                        onChange={handleChange}
-                      />
-                    </CInputGroup>
-                    <CRow> {errorInfo.error && <CAlert color="danger">{errorInfo.errorMessage}</CAlert>} </CRow>
-                    <CRow className="mb-4">
-                      <CCol xs={6}>
-                        <CButton
-                          color="primary"
-                          className="px-4"
-                          onClick={async () => {
-                            let res = await authService.login(form.email, form.password)
-                            if (res.success) {
-                              navigate('/dashboard')
-                            } else {
-                              setErrorInfo({
-                                ...errorInfo,
-                                error: true,
-                                errorMessage: res.errorMessage,
-                              })
-                            }
-                          }}
-                        >
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
+    <IconButton
+      aria-label="toggle light/dark mode"
+      size="sm"
+      variant="outlined"
+      disabled={!mounted}
+      onClick={(event) => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+        setColorMode(mode === 'light' ? 'dark' : 'light')
+        onClick?.(event)
+      }}
+      {...other}
+    >
+      {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
   )
 }
 
-export default Login
+export default function JoySignInSideTemplate() {
+  const authService = AuthService()
+  const navigate = useNavigate()
+
+  return (
+    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ':root': {
+            '--Form-maxWidth': '800px',
+            '--Transition-duration': '0.4s', // set to `none` to disable transition
+          },
+        }}
+      />
+      <Box
+        sx={(theme) => ({
+          width: { xs: '100%', md: '50vw' },
+          transition: 'width var(--Transition-duration)',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          position: 'relative',
+          zIndex: 1,
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backdropFilter: 'blur(12px)',
+          backgroundColor: 'rgba(255 255 255 / 0.2)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundColor: 'rgba(19 19 24 / 0.4)',
+          },
+        })}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100dvh',
+            width: '100%',
+            px: 2,
+          }}
+        >
+          <Box
+            component="header"
+            sx={{
+              py: 3,
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+              <IconButton variant="soft" color="primary" size="sm">
+                <BadgeRoundedIcon />
+              </IconButton>
+              <Typography level="title-lg">Company logo</Typography>
+            </Box>
+            <ColorSchemeToggle />
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              my: 'auto',
+              py: 2,
+              pb: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              width: 400,
+              maxWidth: '100%',
+              mx: 'auto',
+              borderRadius: 'sm',
+              '& form': {
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+              },
+              [`& .MuiFormLabel-asterisk`]: {
+                visibility: 'hidden',
+              },
+            }}
+          >
+            <Stack gap={4} sx={{ mb: 2 }}>
+              <Stack gap={1}>
+                <Typography component="h1" level="h3">
+                  Sign in
+                </Typography>
+              </Stack>
+            </Stack>
+            <Stack gap={4} sx={{ mt: 2 }}>
+              <form
+                onSubmit={async (event) => {
+                  event.preventDefault()
+                  const formElements = event.currentTarget.elements
+                  const data = {
+                    email: formElements.email.value,
+                    password: formElements.password.value,
+                    persistent: formElements.persistent.checked,
+                  }
+                  let res = await authService.login(data.email, data.password)
+                  if (res.success) {
+                    navigate('/dashboard')
+                  }
+                }}
+              >
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" name="email" placeholder="Email" autoComplete="email" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" name="password" placeholder="Password" autoComplete="password" />
+                </FormControl>
+                <Stack gap={4} sx={{ mt: 2 }}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Checkbox size="sm" label="Remember me" name="persistent" />
+                    <Link level="title-sm" href="#replace-with-a-link">
+                      Forgot your password?
+                    </Link>
+                  </Box>
+                  <Button type="submit" fullWidth>
+                    Sign in
+                  </Button>
+                </Stack>
+              </form>
+            </Stack>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" textAlign="center">
+              © Your company {new Date().getFullYear()}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={(theme) => ({
+          height: '100%',
+          position: 'fixed',
+          right: 0,
+          top: 0,
+          bottom: 0,
+          left: { xs: 0, md: '50vw' },
+          transition: 'background-image var(--Transition-duration), left var(--Transition-duration) !important',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          backgroundColor: 'background.level1',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage: 'url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8?auto=format&w=1000&dpr=2)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundImage: 'url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)',
+          },
+        })}
+      />
+    </CssVarsProvider>
+  )
+}
