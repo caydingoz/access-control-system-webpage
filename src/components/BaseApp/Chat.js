ae5b845758bcb7ce36ad79b87f0d7d09344726c8
caydingoz <caydingoz1@gmail.com>
2025-01-26 01:41:24 +0300

    chat date divider added.

--- src/components/BaseApp/Chat.js
+++ src/components/BaseApp/Chat.js
@@ -4,6 +4,7 @@ import Sheet from '@mui/joy/Sheet'
 import IconButton from '@mui/joy/IconButton'
 import Button from '@mui/joy/Button'
 import Input from '@mui/joy/Input'
+import Divider from '@mui/joy/Divider'
 import Typography from '@mui/joy/Typography'
 import List from '@mui/joy/List'
 import ListItem from '@mui/joy/ListItem'
@@ -359,57 +360,80 @@ const Chat = () => {
               }}
             >
               {selectedChat ? (
-                messages.map((message, index) => (
-                  <Box key={index} sx={{ mb: 0.5, textAlign: message.receiverId !== selectedChat.userId ? 'left' : 'right' }}>
-                    <Typography
-                      level="body-sm"
-                      sx={{
-                        display: 'inline-block',
-                        padding: 1,
-                        background:
-                          message.receiverId !== selectedChat.userId
-                            ? theme === 'dark'
-                              ? '#4a3222'
-                              : '#fff3e0'
-                            : theme === 'dark'
-                              ? '#004d4d'
-                              : '#e0f7fa',
-                        borderRadius: 8,
-                        fontSize: '13px',
-                        maxWidth: '75%',
-                        color: theme === 'dark' ? 'white' : 'black',
-                      }}
-                    >
-                      {message.content}
-                    </Typography>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        justifyContent: message.receiverId !== selectedChat.userId ? 'flex-start' : 'flex-end',
-                        alignItems: 'center',
-                        gap: 0.5,
-                      }}
-                    >
-                      <Typography level="body-sm" sx={{ fontSize: '8px' }}>
-                        {TimeZoneConverter.convertUtcToIstanbul(message.sentAt).toLocaleTimeString([], {
-                          hour: '2-digit',
-                          minute: '2-digit',
-                        })}
-                      </Typography>
-                      {message.receiverId === selectedChat.userId &&
-                        (message.isRead === true ? (
-                          <DoneAllIcon sx={{ fontSize: '10px', color: '#4caf50' }} />
-                        ) : (
-                          <DoneAllIcon sx={{ fontSize: '10px', color: 'gray' }} />
-                        ))}
-                    </Box>
-                  </Box>
-                ))
+                messages.map((message, index) => {
+                  const currentMessageDate = new Date(message.sentAt).toLocaleDateString()
+                  const previousMessageDate = index > 0 ? new Date(messages[index - 1].sentAt).toLocaleDateString() : null
+
+                  return (
+                    <React.Fragment key={index}>
+                      {currentMessageDate !== previousMessageDate && previousMessageDate && (
+                        <Divider sx={{ my: 2, color: theme === 'dark' ? 'white' : 'black' }}>
+                          <Typography level="body-sm">{previousMessageDate}</Typography>
+                        </Divider>
+                      )}
+                      <Box
+                        sx={{
+                          mb: 0.5,
+                          textAlign: message.receiverId !== selectedChat.userId ? 'left' : 'right',
+                        }}
+                      >
+                        <Typography
+                          level="body-sm"
+                          sx={{
+                            display: 'inline-block',
+                            padding: 1,
+                            background:
+                              message.receiverId !== selectedChat.userId
+                                ? theme === 'dark'
+                                  ? '#4a3222'
+                                  : '#fff3e0'
+                                : theme === 'dark'
+                                  ? '#004d4d'
+                                  : '#e0f7fa',
+                            borderRadius: 8,
+                            fontSize: '13px',
+                            maxWidth: '75%',
+                            color: theme === 'dark' ? 'white' : 'black',
+                          }}
+                        >
+                          {message.content}
+                        </Typography>
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            justifyContent: message.receiverId !== selectedChat.userId ? 'flex-start' : 'flex-end',
+                            alignItems: 'center',
+                            gap: 0.5,
+                          }}
+                        >
+                          <Typography level="body-sm" sx={{ fontSize: '8px' }}>
+                            {TimeZoneConverter.convertUtcToIstanbul(message.sentAt).toLocaleTimeString([], {
+                              hour: '2-digit',
+                              minute: '2-digit',
+                            })}
+                          </Typography>
+                          {message.receiverId === selectedChat.userId &&
+                            (message.isRead === true ? (
+                              <DoneAllIcon sx={{ fontSize: '10px', color: '#4caf50' }} />
+                            ) : (
+                              <DoneAllIcon sx={{ fontSize: '10px', color: 'gray' }} />
+                            ))}
+                        </Box>
+                      </Box>
+                      {index === messages.length - 1 && (
+                        <Divider sx={{ my: 2, color: theme === 'dark' ? 'white' : 'black' }}>
+                          <Typography level="body-sm">{currentMessageDate}</Typography>
+                        </Divider>
+                      )}
+                    </React.Fragment>
+                  )
+                })
               ) : (
                 <Typography level="body-sm" sx={{ textAlign: 'center' }}>
                   Start a conversation...
                 </Typography>
               )}
+
               <div ref={messagesEndRef} />
             </Sheet>
 
